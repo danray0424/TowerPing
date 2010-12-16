514452932ad5c233cfb089b4359b0e00ed8a2e51
Dan Ray <dray@dynamicquest.com>
2010-12-16 09:23:18 -0500

    first commit

---
+++ main.m
@@ -0,0 +1,1 @@
+////  main.m//  TowerPing////  Created by Daniel Ray on 7/9/10.//  Copyright Dynamic  Quest 2010. All rights reserved.//#import <UIKit/UIKit.h>int main(int argc, char *argv[]) {        NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];    int retVal = UIApplicationMain(argc, argv, nil, nil);    [pool release];    return retVal;}
